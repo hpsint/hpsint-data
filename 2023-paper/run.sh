3b37bcaad85c3d0eadca226a136ab409e064fb90
Peter Munch <peterrmuench@gmail.com>
2023-06-21 20:42:33 +0200

    First commit

---
+++ 2023-paper/run.sh
@@ -0,0 +1,34 @@
+mkdir -p fig-05
+cp ../../dealii-sintering/data/distribution/*.out fig-05
+
+mkdir -p fig-06-fig-19-tab-01-tab-08
+cp ../../dealii-sintering/data/throughput/*.out fig-06-tab-01
+
+mkdir -p fig-07
+cp ../../dealii-sintering/data/amd/*.out fig-07
+
+mkdir -p fig-10-fig-11
+# TODO
+
+mkdir -p fig-12-fig-13-fig-14
+# TODO
+
+mkdir -p tab-5
+cp ../../dealii-sintering/data/comparison-config/*.out tab-5
+
+mkdir -p fig-16
+cp ../../dealii-sintering/data/pie/*.out fig-16
+
+mkdir -p fig-17
+cp ../../dealii-sintering/data/profile/*.out fig-17
+
+mkdir -p tab-6
+cp ../../dealii-sintering/data/comparison-precon/*.out tab-6
+
+mkdir -p tab-7
+cp ../../dealii-sintering/data/comparison-precon-long/*.out tab-7
+
+mkdir -p fig-18
+cp ../../dealii-sintering/data/scaling/*.out fig-18
+
+
