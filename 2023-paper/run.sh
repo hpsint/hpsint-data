01bf7a92bcc5b5b9669af086753903dcd90b59aa
Vladimir Ivannikov <vovannikov@yandex.ru>
2023-10-16 14:41:02 +0200

    Add tab 3 and figs 11-15

--- 2023-paper/run.sh
+++ 2023-paper/run.sh
@@ -31,4 +31,17 @@ mkdir -p fig-19/2
 cp ../../dealii-sintering/data/scaling/2/*.out fig-19/2
 
 
-# TODO: table 3, figure 11-14
+mkdir -p fig-11-fig-12
+cp "../../dealii-sintering/data/5particles/job_hpsint_2d_revision.out" fig-11-fig-12/job_hpsint_2d.out
+cp "../../dealii-sintering/data/5particles/job_hpsint_3d_revision.out" fig-11-fig-12/job_hpsint_3d.out
+cp "../../dealii-sintering/data/5particles/job_moose_2d_revision.out" fig-11-fig-12/job_moose_2d.out
+cp "../../dealii-sintering/data/5particles/job_moose_5p_3d_revision.out" fig-11-fig-12/job_moose_3d.out
+
+mkdir -p tab-3
+cp "../../dealii-sintering/data/5particles/job_hpsint_5p_3d_no_io_output.out" tab-3
+cp "../../dealii-sintering/data/5particles/job_moose_5p_3d_no_io_output.out" tab-3
+cp "../../dealii-sintering/data/5particles/job_hpsint_5p_2d.out" tab-3
+cp "../../dealii-sintering/data/5particles/job_moose_5p_2d.out" tab-3
+
+mkdir -p fig-13-fig-14-fig-15
+cp "../../dealii-sintering/data/332particles/pf_332particles.out" fig-13-fig-14-fig-15
